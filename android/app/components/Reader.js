ac8596866827e09bee6cd7a78206cee5e09f5a5f
ssomnoremac <mosswd@gmail.com>
2016-02-11 16:59:14 -0500

    issue13 - bring RNCouchbaseLite npm module into code under app/vender

--- android/app/components/Reader.js
+++ android/app/components/Reader.js
@@ -8,51 +8,15 @@ var {
   StyleSheet,
   Text,
   View,
-  Platform,
-  TouchableHighlight,
-  TouchableNativeFeedback,
 } = React;
 
 var WebView = require('./WebView');
 
 var Reader = React.createClass({
-  getInitialState() {
-    var imageURI = 'data:image/jpeg;base64,' + this.props.book._attachments["cover.jpg"].data;
-    return {
-      htmlBook: ["<p>htmlBook Page 1</p><img src='" + imageURI + "' />","<p>htmlBook Page 2</p>","<p>htmlBook Page 3</p>"],
-      initialPage: 0,
-    }
-  },
-  //changePage(dir) {
-  //  this.state.initialPage += dir
-  //},
   render() {
-    var TouchableElement = TouchableHighlight;
-    if (Platform.OS === 'android') {
-      TouchableElement = TouchableNativeFeedback;
-    }
+    var bookURL = "https://rawgit.com/InfiniteLibraryLibrary/" + this.props.book.title + "/master/book.xhtml";
     return (
-      <View style={styles.contentContainer}>
-        <WebView html={this.state.htmlBook[this.state.initialPage]}/>
-        <TouchableElement
-          background={TouchableNativeFeedback.Ripple()}
-          >
-          <View style={styles.leftButton} >
-            <View>
-              <Text style={styles.title}>Prev</Text>
-            </View>
-          </View>
-        </TouchableElement>
-        <TouchableElement
-          background={TouchableNativeFeedback.Ripple()}
-          >
-          <View style={styles.rightButton} >
-            <View>
-              <Text style={styles.title}>Next</Text>
-            </View>
-          </View>
-        </TouchableElement>
-      </View>
+      <WebView url={bookURL}/>
     );
   },
 });
@@ -61,45 +25,30 @@ var Reader = React.createClass({
 
 var styles = StyleSheet.create({
   contentContainer: {
-    flex: 1,
+    padding: 10,
   },
-  container: {  
-    flex: 1,
-    flexDirection: 'row',
+  rightPane: {
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  leftButton: {
     flex: 1,
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    backgroundColor: 'gold',
-    opacity: .5,
   },
-  rightButton: {
+  movieTitle: {
     flex: 1,
-    position: 'absolute',
-    top: 0,
-    right: 0,
-    backgroundColor: 'green',
-    opacity: .5,
+    fontSize: 16,
+    fontWeight: '500',
   },
-  title: {
-    fontSize: 20,
-    marginBottom: 8,
-    textAlign: 'center',
+  rating: {
+    marginTop: 10,
   },
-  year: {
-    textAlign: 'center',
-  },
-  navItem: {  
-    flex: 1,
+  mainSection: {
     flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
   },
+  thumbnail: {
+    width: 134,
+    height: 200,
+    backgroundColor: '#eaeaea',
+    marginRight: 10,
+  }
 });
 
 module.exports = Reader;
